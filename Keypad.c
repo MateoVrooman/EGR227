9f14349fb008502dc083d2b0bc9eb66b63565a73
KyleEmersonGVSU <115571221+KyleEmersonGVSU@users.noreply.github.com>
2022-10-13 16:02:57 -0400

    Add files via upload

--- Keypad.c
+++ Keypad.c
@@ -40,11 +40,30 @@ uint8_t Hex2Bit (uint32_t hex_num){
 void KeyPad_Init(){
 	RCC -> AHB1ENR |= 0x14; //Enables GPIOC Clock
 	GPIOC -> MODER &=~ (uint32_t) ((3 << (2 * Hex2Bit(R0))) | (3 << (2 * Hex2Bit(R1))) | (3 << (2*Hex2Bit(R1))) | (3 << (2*Hex2Bit(R2))) | (3 << (2*Hex2Bit(R3))) | (3 << (2*Hex2Bit(C0))) | (3 << (2*Hex2Bit(C1))) | (3 << (2*Hex2Bit(C2))));
-	GPIOC -> PUPDR &=~ (uint32_t) ((3 << (2 * Hex2Bit(R0))) | (3 << (2 * Hex2Bit(R1))) | (3 << (2*Hex2Bit(R1))) | (3 << (2*Hex2Bit(R2))) | (3 << (2*Hex2Bit(R3))) | (3 << (2*Hex2Bit(C0))) | (3 << (2*Hex2Bit(C1))) | (3 << (2*Hex2Bit(C2))));
-	GPIOC -> PUPDR = (uint32_t) ((1 << (2 * Hex2Bit(R0))) | (1 << (2 * Hex2Bit(R1))) | (1 << (2*Hex2Bit(R1))) | (1 << (2*Hex2Bit(R2))) | (1 << (2*Hex2Bit(R3))) | (1 << (2*Hex2Bit(C0))) | (1 << (2*Hex2Bit(C1))) | (1 << (2*Hex2Bit(C2)))); //Enable pull up resistors in columns
+	GPIOC -> PUPDR &=~ (uint32_t) ((3 << (2 * Hex2Bit(R0))) | (3 << (2 * Hex2Bit(R1))) | (3 << (2*Hex2Bit(R1))) | (3 << (2*Hex2Bit(R2))) | (3 << (2*Hex2Bit(R3))));
+	GPIOC -> PUPDR = (uint32_t) ((1 << (2 * Hex2Bit(R0))) | (1 << (2 * Hex2Bit(R1))) | (1 << (2*Hex2Bit(R1))) | (1 << (2*Hex2Bit(R2))) | (1 << (2*Hex2Bit(R3)))); //Enable pull up resistors in columns
 }
-
-uint8_t Read_KeyPad(uint16_t *numpter){
+/***| Read_Keypad() |***********************************//*
+*Brief: Checks for the button press by grounding each col as
+*Brief: assigning each col as GND one by one while the rest
+*Brief: are floating input with infinite impedance. As a result,
+*Brief: if any press is hit, that row will be grounded over the
+*Brief: col that is assigned as GND.
+*|--|--| -> Row0=> GND if 1 is pressed
+*|--|--| -> Row1=> GND if 4 is pressed
+*|--|--| -> Row2=> GND if 7 is pressed
+*|--|--| -> Row3=> GND if * is pressed
+*|=>GND
+*Brief: Caution: If two cols are hit at the same time and they
+*Brief: are assigned as both OUT, there will be a short circuit
+*Brief: that can fry MCU. Hence, multiple repeating IN settings
+*Brief: are placed for safety.
+*Params:
+*           (uint16) *numptr: pressed number pointer
+*Returns:
+*           None
+********************************************************/
+uint8_t Read_KeyPad(uint16_t *numptr){
 	uint8_t col, row;
 	for(col=0; col < 3; col++){
 		GPIOC -> MODER &=~ (uint32_t) ((3 << (2 * Hex2Bit(R0))) | (3 << (2 * Hex2Bit(R1))) | (3 << (2*Hex2Bit(R1))) | (3 << (2*Hex2Bit(R2))) | (3 << (2*Hex2Bit(R3))) | (3 << (2*Hex2Bit(C0))) | (3 << (2*Hex2Bit(C1))) | (3 << (2*Hex2Bit(C2))));
@@ -59,16 +78,38 @@ uint8_t Read_KeyPad(uint16_t *numpter){
 	GPIOC -> MODER &=~ (uint32_t) ((3 << (2 * Hex2Bit(R0))) | (3 << (2 * Hex2Bit(R1))) | (3 << (2*Hex2Bit(R1))) | (3 << (2*Hex2Bit(R2))) | (3 << (2*Hex2Bit(R3))) | (3 << (2*Hex2Bit(C0))) | (3 << (2*Hex2Bit(C1))) | (3 << (2*Hex2Bit(C2))));
 	
 	if(col == 3) return 0;
-	if (row == 0x0E) *numpter = col + 1;			//key in row 0
-	if (row == 0x0D) *numpter = 3 + col + 1;	//key in row 1
-	if (row == 0x0B) *numpter = 6 + col + 1;	//key in row 2
-	if (row == 0x07) *numpter = 9 + col + 1;	//key in row 3
+	if (row == 0x0E) *numptr = col + 1;			//key in row 0
+	if (row == 0x0D) *numptr = col + 4;	//key in row 1
+	if (row == 0x0B) *numptr = col + 7;	//key in row 2
+	if (row == 0x07) *numptr = col + 10;	//key in row 3
 	return 1;
 }
 
-void Print_Keys(uint16_t *numpter){
-	if (*numpter == 12) printf("#\n");
-	if (*numpter == 11) printf("0\n");
-	if (*numpter == 10) printf("0\n");
-	if (*numpter <  10) printf("%d\n", *numpter);
+void Print_Keys(uint16_t *numptr){
+	if (*numptr == 12) printf("#\n");
+	if (*numptr == 11) printf("0\n");
+	if (*numptr == 10) printf("*\n");
+	if (*numptr <  10) printf("%d\n", *numptr);
+}
+/***| PIN_Entry() |*************************************//*
+*Brief: Accepts the 4 digit entry from the user and prints
+*Brief: the numbers together with the final 4-digit PIN.
+*Params:
+*           (uint8_t) pressed: pressed or not
+*           (uint8_t) *digitptr: 4 digit number counter pointer
+*           (uint8_t) pin[]: 4 digit pin array
+*           (uint8_t) turn: turn=1=> first entry, turn=2=> not first entry
+*           (uint16) *numptr: pressed number pointer
+*Returns:
+*           None
+********************************************************/
+void *PIN_Entry (uint16_t *numptr, uint16_t *digitptr, uint8_t *pin, uint8_t turn, uint8_t *exitptr)
+{
+	*exitptr = 0;
+	*pin = *numptr;
+	while(*pin != 10 | *pin != 12);	//sets current pin to entered key while not equal to '*'
+	
+	if(*pin == 12) *exitptr	= 1;
+	
+	*digitptr++;
 }
